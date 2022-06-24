0cb566ef9d1c6983b408c6ba09cfc8d22a0e18a8
Dayanand Challa <dayananda.challa@ngenux.com>
2022-06-24 18:04:55 +0530

    fixing all indentation pylint issue: 1

--- customer_segmentation/kmeans.py
+++ customer_segmentation/kmeans.py
@@ -6,51 +6,51 @@ from kneed import KneeLocator
 
 
 def getBestKWithElbow(K, df):
-	sse = {}
-	for k in K:
-		kmeans = KMeans(n_clusters=k)
-		kmeans.fit(df)
-		sse[k] = kmeans.inertia_
-	#getting optimal cluster value with max score
-	kn = KneeLocator(
-				 x=list(sse.keys()), 
-				 y=list(sse.values()), 
-				 curve='convex', 
-				 direction='decreasing'
-				 )
-	optimal = kn.knee
-	#printing result
-	print('Optimal number of clusters is: ',str(optimal))
-	
+    sse = {}
+    for k in K:
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(df)
+        sse[k] = kmeans.inertia_
+    #getting optimal cluster value with max score
+    kn = KneeLocator(
+                 x=list(sse.keys()), 
+                 y=list(sse.values()), 
+                 curve='convex', 
+                 direction='decreasing'
+                 )
+    optimal = kn.knee
+    #printing result
+    print('Optimal number of clusters is: ',str(optimal))
+    
 def getBestKWithSilhouette(K, df):
-	scores = []
-	for k in K:
-		kmeans = KMeans(n_clusters=k)
-		cls_assignment = kmeans.fit_predict(df)
-		scores.append(silhouette_score(df,cls_assignment))
-	#getting optimal cluster value with max score
-	optimal = K[scores.index(sorted(scores,reverse=True)[0])]
-	#printing result
-	print('Optimal number of clusters is: ',str(optimal))
-	return optimal
+    scores = []
+    for k in K:
+        kmeans = KMeans(n_clusters=k)
+        cls_assignment = kmeans.fit_predict(df)
+        scores.append(silhouette_score(df,cls_assignment))
+    #getting optimal cluster value with max score
+    optimal = K[scores.index(sorted(scores,reverse=True)[0])]
+    #printing result
+    print('Optimal number of clusters is: ',str(optimal))
+    return optimal
 
 
 def predictUsingKMeans(self):
-	print('Predicting with KMeans')
-	#print(self._Id)
-	#print(self._df)
-	#print(self.model_args['K'])
-	if not (self._Id in self._df.columns ):
-		raise Exception(f'Provided ID column {self._Id} is not avialble in the input dataframe!!!')
-	lower = 2
-	upper = self.model_args['K']
-	K=range(lower,upper)
-	df = self._df
-	#print(df.columns)
-	df.drop("CustomerID", axis = 1, inplace=True)
-	optimal_k = getBestKWithElbow(K, df)
-	optimal_k = getBestKWithSilhouette(K, df)
-	kmeans = KMeans(n_clusters=optimal_k)
-	cls_assignment = kmeans.fit_predict(df)
-	self._df['cluster']=cls_assignment
-	return self._df
+    print('Predicting with KMeans')
+    #print(self._Id)
+    #print(self._df)
+    #print(self.model_args['K'])
+    if not (self._Id in self._df.columns ):
+        raise Exception(f'Provided ID column {self._Id} is not avialble in the input dataframe!!!')
+    lower = 2
+    upper = self.model_args['K']
+    K=range(lower,upper)
+    df = self._df
+    #print(df.columns)
+    df.drop("CustomerID", axis = 1, inplace=True)
+    optimal_k = getBestKWithElbow(K, df)
+    optimal_k = getBestKWithSilhouette(K, df)
+    kmeans = KMeans(n_clusters=optimal_k)
+    cls_assignment = kmeans.fit_predict(df)
+    self._df['cluster']=cls_assignment
+    return self._df
--- customer_segmentation/strategy.py
+++ customer_segmentation/strategy.py
@@ -3,20 +3,20 @@ import types
 import pandas as pd
 
 if sys.version_info[0] > 2:  # Python 3+
-	create_bound_method = types.MethodType
+    create_bound_method = types.MethodType
 else:
-	def create_bound_method(func, obj):
-		return types.MethodType(func, obj, obj.__class__)
+    def create_bound_method(func, obj):
+        return types.MethodType(func, obj, obj.__class__)
 
 
 class Strategy:
-	def __init__(self, func=None):
-		self._Id = ""
-		self._df = pd.DataFrame()
-		self.model_args = {}
+    def __init__(self, func=None):
+        self._Id = ""
+        self._df = pd.DataFrame()
+        self.model_args = {}
 
-		if func:
-			self.execute = create_bound_method(func, self)
+        if func:
+            self.execute = create_bound_method(func, self)
 
-	def execute(self):
-		print(self._Id)
+    def execute(self):
+        print(self._Id)
